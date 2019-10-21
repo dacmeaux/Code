03f26ed86902727163f9fff7a10939368029c224
Duane Comeaux <50835556+dacmeaux@users.noreply.github.com>
2019-10-20 23:51:47 -0400

    Part 3 Files

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
-require_once 'class.Data.php';
-require_once 'class.Result.php';
-require_once 'class.Search.php';
 require_once 'class.Response.php';
 require_once 'class.DocumentUtils.php';
-require_once 'class.NumberValidator.php';
 
-$template = file_get_contents('template.html');
 $response = new Response();
 $data = $response->getData();
 $documents = DocumentUtils::parseResponseToDocuments($response);
